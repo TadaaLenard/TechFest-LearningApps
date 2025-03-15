ea5842309a14d74d67a870a0d8eadbdde3bf33d4
TadaaLenard <lenard8794@gmail.com>
2025-03-15 16:46:28 +0800

    done homepage design

--- ilearn/lib/homepage.dart
+++ ilearn/lib/homepage.dart
@@ -25,159 +25,206 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      body: SingleChildScrollView(
-        // Makes the screen scrollable
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 50, // Set height
-                color: Colors.purple, // Set background color
-              ),
-              Container(
-                color: const Color.fromARGB(
-                    255, 250, 221, 255), // Background color for the row
-                padding: EdgeInsets.all(10), // Optional padding
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        print("Button Pressed!");
-                      },
-                      child: Icon(
-                        Icons.menu,
-                      ),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.5,
-                      child: ElevatedButton(
+      body: Theme(
+        data: Theme.of(context).copyWith(
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              textStyle:
+                  TextStyle(fontSize: 20), // Change only for this Scaffold
+            ),
+          ),
+        ),
+        child: SingleChildScrollView(
+          // Makes the screen scrollable
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 50, // Set height
+                  color: Colors.purple, // Set background color
+                ),
+                Container(
+                  color: const Color.fromARGB(
+                      255, 250, 221, 255), // Background color for the row
+                  padding: EdgeInsets.all(10), // Optional padding
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
                         onPressed: () {
                           print("Button Pressed!");
                         },
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
+                        icon: Icon(
+                          Icons.menu,
+                          size: 50,
+                          color: Colors.black, // Adjust color as needed
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            print("Button Pressed!");
+                          },
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Icon(
+                                  Icons.search,
+                                ),
+                              ]),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          print("Button Pressed!");
+                        },
+                        icon: Icon(
+                          size: 50,
+                          Icons.account_circle,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 12, horizontal: 16), // Adjust padding
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                  16), // Adjust border radius
+                            ),
+                          ),
+                          child: const Column(
+                            mainAxisSize:
+                                MainAxisSize.min, // Keep button compact
                             children: [
-                              Icon(
-                                Icons.search,
-                              ),
-                            ]),
+                              Icon(Icons.desk_rounded,
+                                  color: Colors.purple,
+                                  size: 70), // Adjust size if needed
+                              SizedBox(
+                                  height: 4), // Space between icon and text
+                              Text("Workshop"),
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        print("Button Pressed!");
-                      },
-                      child: Icon(
-                        Icons.account_circle,
+                      SizedBox(
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            print("Button Pressed!");
+                          },
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 12, horizontal: 16), // Adjust padding
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                  16), // Adjust border radius
+                            ),
+                          ),
+                          child: const Column(children: [
+                            Icon(Icons.video_call_rounded,
+                                color: Colors.purple,
+                                size: 70), // Adjust size if needed
+                            SizedBox(height: 4), Text("Tutorial")
+                          ]),
+                        ),
                       ),
-                    )
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton.icon(
-                      icon: Icon(Icons.thumb_up, color: Colors.white),
-                      onPressed: () {
-                        print("Button Pressed!");
-                      },
-                      label: Text("Workshop"),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        print("Button Pressed!");
-                      },
-                      child: Text("Tutorial"),
-                    ),
-                  ],
+                Divider(
+                  color: Colors.black, // Line color
+                  thickness: 1, // Line thickness
+                  height: 20, // Space around the divider
                 ),
-              ),
-              Divider(
-                color: Colors.black, // Line color
-                thickness: 1, // Line thickness
-                height: 20, // Space around the divider
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Sciences",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Physics",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10),
-                    ScrollableButtonList(
-                      items: [
-                        "Gravity 1",
-                        "Gravity 2",
-                        "Gravity 3",
-                        "Gravity 3",
-                        "Gravity 3"
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    SizedBox(height: 10),
-                    Text(
-                      "Biology",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10),
-                    ScrollableButtonList(
-                      items: [
-                        "Gravity 1",
-                        "Gravity 2",
-                        "Gravity 3",
-                        "Gravity 3",
-                        "Gravity 3"
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Language",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Chinese",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10),
-                    ScrollableButtonList(
-                      items: [
-                        "Gravity 1",
-                        "Gravity 2",
-                        "Gravity 3",
-                        "Gravity 3",
-                        "Gravity 3"
-                      ],
-                    ),
-                  ],
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Sciences",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Physics",
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(height: 10),
+                      ScrollableButtonList(
+                        items: [
+                          "Gravity 1",
+                          "Gravity 2",
+                          "Force 1",
+                          "Force 2",
+                          "Electromagnetic"
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      SizedBox(height: 10),
+                      Text(
+                        "Biology",
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(height: 10),
+                      ScrollableButtonList(
+                        items: [
+                          "Photosynthesis",
+                          "Cell Organisation",
+                          "Nutrition",
+                          "Reproduction",
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Language",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "English",
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(height: 10),
+                      ScrollableButtonList(
+                        items: [
+                          "Pronunciation",
+                          "Grammar 1",
+                          "Grammar 2",
+                          "Grammar 3",
+                          "Formal Writing"
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -198,19 +245,23 @@ class ScrollableButtonList extends StatelessWidget {
         children: items.map((item) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                print("$item Clicked");
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.grey[300], // Light grey color
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20), // Rounded corners
+            child: SizedBox(
+              height: 70,
+              child: ElevatedButton(
+                onPressed: () {
+                  print("$item Clicked");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(
+                      255, 255, 234, 234), // Light grey color
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20), // Rounded corners
+                  ),
                 ),
-              ),
-              child: Text(
-                item,
-                style: TextStyle(color: Colors.black), // Text color
+                child: Text(
+                  item,
+                  style: TextStyle(color: Colors.black), // Text color
+                ),
               ),
             ),
           );
